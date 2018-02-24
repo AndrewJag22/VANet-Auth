bd3cd16c13a262faa7b231c13716a605ed846722
Shreyas Bhat <shreyasnbhat@gmail.com>
2018-02-24 23:33:28 +0530

    Added intial vehicle auth phase

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ VEHICLE_COUNT = 1
 RSU_COUNT = 6
 TRAFFIC_AUTHORITY_ID = "TA-001"
 
-if __name__ == '__main__':
+
+def simulate(iterations, skips, hash_size):
+    print("------ Simulation for key size " + str(hash_size) + " begins -------")
 
     times = []
 
-    for j in range(100, 1000, 100):
+    # Set hash_size value
+    Base.hash_size = hash_size
+    ticks = [j for j in range(1, iterations, skips)]
+
+    for j in range(1, iterations, skips):
 
         ta = TrafficAuthority(TRAFFIC_AUTHORITY_ID)
 
@@ -18,13 +24,28 @@ if __name__ == '__main__':
         init = time.time()
 
         for i in range(j):
-            passwd = Base.generate_random_nonce()
-            r = Base.generate_160bit_key()
-            k = Base.generate_160bit_key()
+            passwd = Base.byte_to_string(Base.generate_random_nonce(Base.hash_size))
+            r = Base.generate_key(Base.hash_size)
+            k = Base.generate_key(Base.hash_size)
             vehicle = Vehicle(user_ids[i], str(i), passwd, r, k)
             vehicle.request_registration(ta)
+            vehicle.vehicle_authenticate(str(i),passwd)
 
         fin = time.time()
         print("Time for", j, "vehicles:", fin - init, "Hashed Computed:", 17 * j, "\tTime/Hash:",
               (fin - init) / (17 * j))
         times.append(fin - init)
+
+    print("------ Simulation for key size " + str(hash_size) + " ends -------")
+    return ticks, times
+
+
+if __name__ == '__main__':
+    ticks1, times1 = simulate(100, 1, 160)
+    ticks2, times2 = simulate(100, 1, 256)
+    ticks3, times3 = simulate(100, 1, 512)
+
+    mp.plot(ticks1, times1)
+    mp.plot(ticks2, times2)
+    mp.plot(ticks3, times3)
+    mp.show()
--- models.py
+++ models.py
@@ -1,21 +1,31 @@
 import hashlib
 import time
 import random
+import os
 
 
 class Base:
+    hash_size = 160
+
     def __init__(self):
         pass
 
     @staticmethod
-    def hash(hash_input):
+    def hash(hash_input, size):
         '''
         Computes a 160bit hash value for the given input
         :param hash_input: String to be hashed
         :return: 160bit hash value
         '''
-        hash_input = hash_input.encode('utf-8')
-        return hashlib.sha1(hash_input).hexdigest()
+        if type(hash_input) is not bytes:
+            hash_input = hash_input.encode('utf-8')
+
+        if size == 160:
+            return hashlib.sha1(hash_input).hexdigest()
+        elif size == 256:
+            return hashlib.sha256(hash_input).hexdigest()
+        elif size == 512:
+            return hashlib.sha512(hash_input).hexdigest()
 
     @staticmethod
     def sxor(x, y):
@@ -26,25 +36,33 @@ class Base:
         return ''.join(chr(ord(a) ^ ord(b)) for a, b in zip(x, y))
 
     @staticmethod
-    def generate_random_nonce():
+    def generate_random_nonce(size):
         '''
-        :return: A random 160bit random nonce
+        :return: A random nonce based on the number of bits
         '''
-        return str(random.randint(0, 10000000000000000))
+        return os.urandom(size // 16)
 
     @staticmethod
-    def generate_160bit_key():
-        hash_input = Base.generate_random_nonce().encode('utf-8')
-        return hashlib.sha1(hash_input).hexdigest()
+    def generate_key(size):
+        hash_input = Base.generate_random_nonce(size)
+        if size == 160:
+            return hashlib.sha1(hash_input).hexdigest()
+        elif size == 256:
+            return hashlib.sha256(hash_input).hexdigest()
+        elif size == 512:
+            return hashlib.sha512(hash_input).hexdigest()
 
     @staticmethod
     def generate_current_timestamp():
-        # Returns time since epoch (UTC)
         return str(int(time.time()))
 
     @staticmethod
     def hex_transform(x):
         return ":".join("{:02x}".format(ord(c)) for c in x)
+
+    @staticmethod
+    def byte_to_string(x):
+        return "".join(chr(c) for c in x)
 
 
 class TrafficAuthority(Base):
@@ -61,29 +79,24 @@ class TrafficAuthority(Base):
 
     def register_vehicle(self, vehicle_id, user_id, masked_password_xor_k, registration_time):
         self.registered_cars[vehicle_id] = registration_time
-        pseudo_id = self.hash(vehicle_id + self.X)
-        a1 = self.hash(pseudo_id + self.X)
-        a2 = self.hash(pseudo_id + a1 + self.id)
-        x = self.hash(self.id + self.X)
-        x_dash = self.hash(self.id + self.X_dash)
+        pseudo_id = self.hash(vehicle_id + self.X, self.hash_size)
+        a1 = self.hash(pseudo_id + self.X, self.hash_size)
+        a2 = self.hash(pseudo_id + a1 + self.id, self.hash_size)
+        x = self.hash(self.id + self.X, self.hash_size)
+        x_dash = self.hash(self.id + self.X_dash, self.hash_size)
         y1 = self.sxor(self.sxor(x, a2), masked_password_xor_k)
         y2 = self.sxor(self.sxor(x_dash, a2), masked_password_xor_k)
-        k_v = self.generate_160bit_key()
-        temporal_credential = self.hash(k_v + str(registration_time) + user_id)
+        k_v = self.generate_key(160)
+        temporal_credential = self.hash(k_v + str(registration_time) + user_id, self.hash_size)
         return pseudo_id, temporal_credential, self.id, y1, y2, a1, a2
 
     # Need to improve this function
     def generate_1024bit_secret_keys(self):
-        x1 = self.sha512(self.generate_random_nonce())
-        x2 = self.sha512(self.generate_random_nonce())
-        x1_dash = self.sha512(self.generate_random_nonce())
-        x2_dash = self.sha512(self.generate_random_nonce())
+        x1 = self.generate_key(512)
+        x2 = self.generate_key(512)
+        x1_dash = self.generate_key(512)
+        x2_dash = self.generate_key(512)
         return x1 + x2, x1_dash + x2_dash
-
-    @staticmethod
-    def sha512(hash_input):
-        hash_input = hash_input.encode('utf-8')
-        return hashlib.sha512(hash_input).hexdigest()
 
 
 class RSU(Base):
@@ -102,9 +115,9 @@ class RSU(Base):
     def register(self, traffic_authority):
         self.registration_time = str(time.time())
         x, x_dash = traffic_authority.X, traffic_authority.X_dash
-        self.pseudo_id = self.hash(self.id + x_dash)
-        self.secret_key = self.hash(traffic_authority.id + x_dash)
-        self.time_dependent_id = self.hash(traffic_authority.id + self.registration_time + x_dash)
+        self.pseudo_id = self.hash(self.id + x_dash, self.hash_size)
+        self.secret_key = self.hash(traffic_authority.id + x_dash, self.hash_size)
+        self.time_dependent_id = self.hash(traffic_authority.id + self.registration_time + x_dash, self.hash_size)
 
 
 class Vehicle(Base):
@@ -127,16 +140,40 @@ class Vehicle(Base):
 
     def request_registration(self, traffic_authority):
         self.registration_time = time.time()
-        masked_password = self.hash(self.password + self.r)
+        masked_password = self.hash(self.password + self.r, self.hash_size)
         masked_password_xor_k = self.sxor(masked_password, self.k)
-        pseudo_id, temporal_credential, traffic_authority_id, y1, y2, a1, a2 = traffic_authority.register_vehicle(
-            self.id, self.user_id, masked_password_xor_k, self.registration_time)
-        self.b = self.sxor(self.hash(self.password + self.id), self.r)
-        self.a1_dash = self.sxor(a1, self.hash(self.id + self.r))
-        self.traffic_authority_id_dash = self.sxor(self.hash(self.id + self.r), traffic_authority.id)
-        a3 = self.hash(self.id + masked_password + traffic_authority.id + a1)
-        self.a4 = self.hash(a3 + a2)
-        self.pseudo_id_dash = self.sxor(pseudo_id, self.hash(self.password + self.id + self.r))
-        self.temporal_credential_dash = self.sxor(temporal_credential, self.hash(self.password + self.r))
+        pseudo_id, temporal_credential, traffic_authority_id, y1, y2, a1, a2 = \
+            traffic_authority.register_vehicle(self.id,
+                                               self.user_id,
+                                               masked_password_xor_k,
+                                               self.registration_time)
+
+        self.b = self.sxor(self.hash(self.password + self.id, self.hash_size), self.r)
+        self.a1_dash = self.sxor(a1, self.hash(self.id + self.r, self.hash_size))
+        self.traffic_authority_id_dash = self.sxor(self.hash(self.id + self.r, self.hash_size), traffic_authority.id)
+        a3 = self.hash(self.id + masked_password + traffic_authority.id + a1, self.hash_size)
+        self.a4 = self.hash(a3 + a2, self.hash_size)
+        self.pseudo_id_dash = self.sxor(pseudo_id, self.hash(self.password + self.id + self.r, self.hash_size))
+        self.temporal_credential_dash = self.sxor(temporal_credential,
+                                                  self.hash(self.password + self.r, self.hash_size))
         self.y = self.sxor(y1, self.k)
         self.y_dash = self.sxor(y2, self.k)
+
+    def vehicle_authenticate(self, id, password):
+        r_star = self.sxor(self.b, self.hash(password + id, self.hash_size))
+        a1_star = self.sxor(self.a1_dash, self.hash(id + r_star, self.hash_size))
+        masked_password_star = self.hash(password + r_star, self.hash_size)
+        traffic_authority_id_star = self.sxor(self.traffic_authority_id_dash, self.hash(id + r_star, self.hash_size))
+        pseudo_id = self.sxor(self.pseudo_id_dash, self.hash(password + id + r_star, self.hash_size))
+        a2_star = self.hash(pseudo_id + a1_star + traffic_authority_id_star, self.hash_size)
+        x = self.sxor(self.y, a2_star)
+        x_dash = self.sxor(self.sxor(self.y_dash, a2_star), masked_password_star)
+        a3_star = self.hash(id + masked_password_star + traffic_authority_id_star + a1_star, self.hash_size)
+        a4_star = self.hash(a3_star + a2_star, self.hash_size)
+        temporal_credential = self.sxor(self.temporal_credential_dash,masked_password_star)
+
+        if a4_star == self.a4:
+            print("Authenticated!")
+        else:
+            print("Can't Authenticate! Could not proceed")
+            return
